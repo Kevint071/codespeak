806ef6cc31b7f808ead95ae40a3da00928e97bac
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2024-10-25 19:05:32 -0500

    feat(name, username, email): Nuevas validaciones

--- src/validations/registerSchema.ts
+++ src/validations/registerSchema.ts
@@ -4,16 +4,29 @@ export const registerSchema = z
 	.object({
 		name: z
 			.string()
-			.min(4, { message: "El nombre debe tener al menos 4 caracteres" })
-			.max(60, { message: "El nombre no puede tener más de 60 caracteres" }),
+			.min(1, { message: "El nombre es obligatorio." })
+			.max(60, { message: "El nombre no puede tener más de 60 caracteres." })
+			.refine((val) => val.length >= 4 || val.length === 0, {
+				message: "El nombre debe tener al menos 4 caracteres.",
+			}),
 		username: z
 			.string()
-			.min(2, { message: "El nombre de usuario debe tener al menos 2 caracteres" })
-			.max(50, { message: "El nombre de usuario no puede tener más de 50 caracteres" })
+			.min(2, {
+				message: "El nombre de usuario debe tener al menos 2 caracteres.",
+			})
+			.max(50, {
+				message: "El nombre de usuario no puede tener más de 50 caracteres.",
+			})
+			.refine((val) => val.trim() !== "", {
+				message: "El nombre de usuario es obligatorio.",
+			})
 			.refine((val) => !val.includes(" "), {
-				message: "El nombre de usuario no puede contener espacios",
+				message: "El nombre de usuario no puede contener espacios.",
 			}),
-		email: z.string().email({ message: "El email no es válido" }),
+		email: z
+			.string()
+			.min(1, { message: "El email es obligatorio" })
+			.email({ message: "El email no es válido" }),
 		password: z
 			.string()
 			.min(8, { message: "La contraseña debe tener al menos 8 caracteres" }),
