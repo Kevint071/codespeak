ab3cfa837e98c62869a04f2de6f1146b1661298b
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2024-11-01 15:37:33 -0500

    feat: agrega Suspense para ServerNavbarContent y reemplaza NavBarContent

--- src/app/recursos/[tech]/[slugTech]/page.jsx
+++ src/app/recursos/[tech]/[slugTech]/page.jsx
@@ -1,22 +1,26 @@
 import { Suspense } from "react";
-import { getInfoNavBar } from "@/services/getDataResources";
 import MainContent from "@/app/recursos/[tech]/[slugTech]/MainContent";
-import NavBarContent from "@/app/recursos/[tech]/[slugTech]/NavBarContent";
 import MainContentSkeleton from "@/app/recursos/[tech]/[slugTech]/MainContentSkeleton";
+import NavBarContentSkeleton from "@/app/recursos/[tech]/[slugTech]/NavBarContentSkeleton";
+import ServerNavbarContent from "@/app/recursos/[tech]/[slugTech]/ServerNavbarContent";
 
 export default async function Component({ params }) {
 	const categoria = params.tech;
 	const enlaceNavBar = params.slugTech;
-	const topics = await getInfoNavBar(categoria);
 
 	return (
-		<div className="mx-auto container max-w-7xl">
-			<div className="flex justify-center gap-x-8 text-gray-300 px-10">
-				<NavBarContent topics={topics} />
-				<Suspense fallback={<MainContentSkeleton />}>
-					<MainContent slug={enlaceNavBar} categoria={categoria} />
-				</Suspense>
-			</div>
+		<div className="container mx-auto max-w-7xl">
+				<div className="flex justify-center gap-x-8 px-10 text-gray-300">
+					<Suspense fallback={<NavBarContentSkeleton />}>
+						<ServerNavbarContent categoria={categoria} />
+					</Suspense>
+					<Suspense fallback={<MainContentSkeleton />}>
+						<MainContent
+							slug={enlaceNavBar}
+							categoria={categoria}
+						/>
+					</Suspense>
+				</div>
 		</div>
 	);
 }
