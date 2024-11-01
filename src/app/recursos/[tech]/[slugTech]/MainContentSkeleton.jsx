26e9c324fbbc83100ebddf67f6f91eda99e92f23
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2024-11-01 15:35:38 -0500

    style: agrega clase mt-4 a párrafo en MainContentSkeleton

--- src/app/recursos/[tech]/[slugTech]/MainContentSkeleton.jsx
+++ src/app/recursos/[tech]/[slugTech]/MainContentSkeleton.jsx
@@ -1,7 +1,11 @@
 function MainContentSkeleton() {
 	return (
 		<main className="mx-4 min-h-[calc(100vh-5rem)] w-full">
-			<p className="mx-auto w-full animate-pulse py-10 sm:w-[500px] md:w-[690px] lg:w-[600px] xl:w-[768px]">Debido a el uso de la versión <strong>Free</strong> de Render con Strapi, esta solicitud puede durar hasta 2 minutos solo la primera vez</p>
+			<p className="mx-auto mt-4 w-full animate-pulse py-10 sm:w-[500px] md:w-[690px] lg:w-[600px] xl:w-[768px]">
+				Debido a el uso de la versión <strong>Free</strong> de Render
+				con Strapi, esta solicitud puede durar hasta 2 minutos solo la
+				primera vez
+			</p>
 			<div className="mx-auto w-full animate-pulse sm:w-[500px] md:w-[690px] lg:w-[600px] xl:w-[768px]">
 				{/* Skeleton para el título */}
 				<div className="mb-10 mt-1 h-8 w-2/4 rounded bg-[#ff6f91]/70" />
