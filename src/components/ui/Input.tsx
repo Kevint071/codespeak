3b0fd2283253c2cf9fbffcdd2273e19a858ff4be
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2024-12-04 23:05:27 -0500

    refactor(Input): reestructurar componente Input para mejor flexibilidad y estilo

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,26 +1,25 @@
-import React from "react";
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { cn } from "@/lib/utils";
+import * as React from "react";
 
-interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
-	className?: string; // Agregué este parámetro
-}
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-9 w-full rounded-lg border border-input bg-background px-3 py-2 text-sm text-foreground shadow-sm shadow-black/5 transition-shadow placeholder:text-muted-foreground/70 focus-visible:border-ring focus-visible:outline-none focus-visible:ring-[3px] focus-visible:ring-ring/20 disabled:cursor-not-allowed disabled:opacity-50",
+          type === "search" &&
+            "[&::-webkit-search-cancel-button]:appearance-none [&::-webkit-search-decoration]:appearance-none [&::-webkit-search-results-button]:appearance-none [&::-webkit-search-results-decoration]:appearance-none",
+          type === "file" &&
+            "p-0 pr-3 italic text-muted-foreground/70 file:me-3 file:h-full file:border-0 file:border-r file:border-solid file:border-input file:bg-transparent file:px-3 file:text-sm file:font-medium file:not-italic file:text-foreground",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = "Input";
 
-export const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
-	const { className, ...restProps } = props; // Desestructuré los props
-
-	return (
-		<input
-			className={twMerge(
-				clsx(
-					"mt-1 block w-full rounded-md border border-gray-600 bg-gray-700 px-3 py-2 text-white placeholder-gray-400 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-[#00ffff]",
-					className, // Agregué className aquí
-				),
-			)}
-			{...restProps} // Usé restProps en lugar de props
-			ref={ref}
-		/>
-	);
-});
-
-export default Input;
+export { Input };
