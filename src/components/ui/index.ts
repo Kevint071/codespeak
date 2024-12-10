da10d1e1552292dc03ac7c36f535b944b3430b1a
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2024-12-09 19:45:08 -0500

    chore: Actualizar nombre componentes base

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,3 @@
-export { Input } from "./input";
-export { Label } from "./label";
 export { MessageError } from "./MessageError";
 export { H1 } from "./H1";
 export { BotonLuminoso } from "./BotonLuminoso";
@@ -7,3 +5,5 @@ export { RegistroGoogle, RegistroGitHub } from "./RegistrosAlternativos";
 export { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 export { BotonEditCheck } from "./BotonEditCheck";
 export { CopyButton } from "./CopyButton";
+export { Input } from "./input-base";
+export { Label } from "./label-base";
